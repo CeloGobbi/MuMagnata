e4eb682fda65e05fb61e31031e7f0b4f052d1bd5
Mushu949 <marcelogobbi13@gmail.com>
2017-07-05 13:19:11 -0300

    beta 2 pre 1.0

--- src/atomiccraft/com/br/Comandos.java
+++ src/atomiccraft/com/br/Comandos.java
@@ -6,7 +6,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 
-public class Comandos implements Listener, CommandExecutor{
+public class Comandos implements CommandExecutor, Listener{
 	public String magnataAtual;
 	public String magnataAntigo;
 	public String titleMagnataLogou;
