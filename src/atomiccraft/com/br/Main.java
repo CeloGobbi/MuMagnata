85f426cc07d8e476b73492829dfca89ad823979b
Mushu949 <marcelogobbi13@gmail.com>
2017-07-06 03:08:38 -0300

    Release 1.0 final

--- src/atomiccraft/com/br/Main.java
+++ src/atomiccraft/com/br/Main.java
@@ -6,9 +6,15 @@ import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -16,43 +22,33 @@ import org.bukkit.scheduler.BukkitRunnable;
 import de.Herbystar.TTA.TTA_Methods;
 import net.milkbowl.vault.economy.Economy;
 
-public class Main extends JavaPlugin implements Listener{
-	public String magnataAtual;
-	public String magnataAntigo;
-	public String titleMagnataLogou;
-	public String subtitleMagnataLogou;
-	public String msgMagnataLogou;
-	public String titleMagnataDeslogou;
-	public String subtitleMagnataDeslogou;
-	public String msgMagnataDeslogou;
-	public String msgComandoMagnata;
-	public static final Logger log = Logger.getLogger("Minecraft");
-    public static Economy econ = null;
+public class Main extends JavaPlugin implements Listener,CommandExecutor{
+	private static final Logger log = Logger.getLogger("Minecraft");
+	private static Economy econ = null;
     
 
 	@Override
     public void onEnable(){
-	    Bukkit.getServer().getPluginManager().registerEvents(new Comandos(this, magnataAtual, magnataAntigo, msgComandoMagnata), this);
-	    Bukkit.getServer().getPluginManager().registerEvents(new MagnataLogin(this, magnataAntigo, magnataAntigo, magnataAntigo, magnataAntigo, magnataAntigo, magnataAntigo, magnataAntigo, magnataAntigo), this);
-	    getCommand("magnata").setExecutor(new Comandos(this, magnataAntigo, magnataAntigo, magnataAntigo));
-	    if ((Bukkit.getPluginManager().getPlugin("Vault") == null) || 
-	      (!Bukkit.getPluginManager().getPlugin("Vault").isEnabled()))
-	    {
-	      Bukkit.getLogger().warning(getConfig().getString("Prefixo-Do-Plugin") + "Vault não encontrado!");
-	    }
-	    else
-	    {
-	      setupEconomy();
-	      Bukkit.getLogger().info("Vault encontrado com sucesso.");
-	    }
+		getServer().getPluginCommand("magnata").setExecutor(this);
+	if ((Bukkit.getPluginManager().getPlugin("Vault") == null) || 
+		(!Bukkit.getPluginManager().getPlugin("Vault").isEnabled()))
+
+    {
+      Bukkit.getLogger().warning(getConfig().getString("Prefixo-Do-Plugin") + "Vault não encontrado!");
+    }
+    else
+    {
+      setupEconomy();
+      Bukkit.getLogger().info("Vault encontrado com sucesso.");
+    }
     	if (!setupEconomy()) {
             log.severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
             getServer().getPluginManager().disablePlugin(this);
             return;
-            
         }
+    getServer().getConsoleSender().sendMessage("§3Plugin: §6MuMagnata §2- §3Desenvolvedor: §6Mushu949 §2 §3Prefixo: ["+ getConfig().getString("Prefixo-Do-Plugin") +"]");
 
-if (!new File(getDataFolder(), "config.yml").exists()){
+  if (!new File(getDataFolder(), "config.yml").exists()){
 	saveResource("config.yml", false);
 	getServer().getConsoleSender().sendMessage("§3" + getConfig().getString("Prefixo-Do-Plugin")+ " " +"§6Verificando config...");
 	getServer().getConsoleSender().sendMessage("§3" + getConfig().getString("Prefixo-Do-Plugin")+ " " +"§4ERRO: Config inexistente! §aCriando aquivo config.yml...");
@@ -72,41 +68,12 @@ if (!new File(getDataFolder(), "config.yml").exists()){
 			Checar();
 			}
 	}.runTaskLater(this, 10*20);
-
-	getServer().getConsoleSender().sendMessage("§3Plugin: §6MuMagnata §2- §3Desenvolvedor: §6Mushu949 §2 §3Prefixo: ["+ getConfig().getString("Prefixo-Do-Plugin") +"]");
 	getServer().getConsoleSender().sendMessage("§3" + getConfig().getString("Prefixo-Do-Plugin")+ " " +"§6Plugin habilitado!");
 	getServer().getConsoleSender().sendMessage("§3" + getConfig().getString("Prefixo-Do-Plugin")+ " " +"§6Magnata atual: §2" + getConfig().getString("Magnata_atual"));			            
-	String MagnataAtual = getConfig().getString("Magnata_atual", "Mushu949");
-	String MagnataAntigo = getConfig().getString("Magnata-antigo", "Mushu");
-	String titleMagnataLogou = getConfig().getString("title-magnata-logou", "§2Magnata");
-	String subtitleMagnataLogou = getConfig().getString("subtitle-magnata-logou", "§2O magnata §f@magnata §2logou no servidor!");
-	String msgMagnataLogou = getConfig().getString("msg-magnata-logou", "§2O magnata §f@magnata §2logou no servidor!");
-	String titleMagnataDeslogou = getConfig().getString("title-magnata-deslogou", "§2Magnata");
-	String subtitleMagnataDeslogou = getConfig().getString("subtitle-magnata-deslogou", "§2O magnata §f@magnata §2deslogou o servidor");
-	String msgMagnataDeslogou = getConfig().getString("title-magnata-deslogou", "§2O magnata §f@magnata §2deslogou o servidor");
-	String msgComandoMagnata = getConfig().getString("msg-comando-magnata", "§2O magnata atual é: §f@exmagnata");
-    new MagnataLogin
-    (
-    		this,
-    		MagnataAtual,
-    		MagnataAntigo,
-    		titleMagnataLogou,
-    		subtitleMagnataLogou,
-    		msgMagnataLogou,
-    		titleMagnataDeslogou,
-    		subtitleMagnataDeslogou,
-    		msgMagnataDeslogou
-    	);
-    new Comandos
-    (
-    		this,
-    		MagnataAtual,
-    		MagnataAntigo,
-    		msgComandoMagnata
-    	);
+	getServer().getPluginManager().registerEvents(this, this);	
 }
 
-    }
+}
 
 public void onDisable(){;
 log.info(String.format("[%s] Disabled Version %s", getDescription().getName(), getDescription().getVersion()));
@@ -177,7 +144,15 @@ HandlerList.unregisterAll();
 			if(novo == true){
 				Collection<? extends Player> list = Bukkit.getOnlinePlayers();
 				for (Player p : list) {
-					TTA_Methods.sendTitle(p, getConfig().getString("title-novo-magnata").replaceAll("@magnata", getConfig().getString("Magnata_atual")).replaceAll("@exmagnata", getConfig().getString("Magnata-antigo")), 5, 100, 5, getConfig().getString("subtitle-novo-magnata").replaceAll("@magnata", getConfig().getString("Magnata_atual")).replaceAll("@exmagnata", getConfig().getString("Magnata-antigo")), 5, 100, 5);
+					TTA_Methods.sendTitle(//API
+							p, getConfig().getString("title-novo-magnata")
+							.replaceAll("@magnata", getConfig().getString("Magnata_atual"))
+							.replaceAll("@exmagnata", getConfig().getString("Magnata-antigo"))
+							, 5, 100, 5 //Tempo do title.
+							, getConfig().getString("subtitle-novo-magnata")
+							.replaceAll("@magnata", getConfig().getString("Magnata_atual"))
+							.replaceAll("@exmagnata", getConfig().getString("Magnata-antigo"))
+							, 5, 100, 5); //Tempo do subtitle.
 				}
 				Bukkit.getServer().broadcastMessage("");
 				Bukkit.getServer().broadcastMessage(getConfig().getString("msg-novo-magnata01").replaceAll("@magnata", getConfig().getString("Magnata_atual")).replaceAll("@exmagnata", getConfig().getString("Magnata-antigo")));
@@ -206,7 +181,75 @@ HandlerList.unregisterAll();
 
 				}
 		              }
-        }.runTaskLater(this, 10*20);
+        }.runTaskLater(this, 10*20); //Temporizador que verifica se existe um nono magnata, ex1: 10*20 = depois de 10 segundo verificar. ex2: 300*20 = depois de 300 segundos verificar.
+	}
+
+	@Override
+	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
+		if(!(arg0 instanceof Player)) {
+			arg0.sendMessage("§2"+ getConfig().getString("Prefixo-Do-Plugin") + " " + getConfig().getString("ERRO01"));
+			return true;
+		}
+	    if (arg1.getName().equalsIgnoreCase("magnata")) {//comando
+	      Player p = (Player)arg0;
+	      if (getConfig().getString("Magnata_atual") == null)
+	      {
+		    p.sendMessage("");
+	        p.sendMessage(getConfig().getString("msg-null-comando-magnata").replaceAll("@magnata", getConfig().getString("Magnata_atual")).replaceAll("@exmagnata", getConfig().getString("Magnata-antigo")));
+		    p.sendMessage("");
+	        return true;
+	      }
+	      p.sendMessage("");
+	      p.sendMessage("§2"+ getConfig().getString("Prefixo-Do-Plugin") + " " + getConfig().getString("msg-comando-magnata").replaceAll("@magnata", getConfig().getString("Magnata_atual")).replaceAll("@exmagnata", getConfig().getString("Magnata-antigo")));
+	      p.sendMessage("");
+
+	        return true;
+	    }
+		return false;
+}
+	 
+	@EventHandler
+	public void Magnata_logando(PlayerJoinEvent e){
+		Player p = e.getPlayer();
+		if(p.getName().equalsIgnoreCase(getConfig().getString("Magnata_atual"))){
+		Collection<? extends Player> list = Bukkit.getOnlinePlayers();
+		for (Player p2 : list) {
+			TTA_Methods.sendTitle(//API
+					p2, getConfig().getString("title-magnata-logou")
+					.replaceAll("@magnata", getConfig().getString("Magnata_atual"))
+					.replaceAll("@exmagnata", getConfig().getString("Magnata-antigo"))
+					, 5, 40, 5 //Tempo do title.
+					, getConfig().getString("subtitle-magnata-logou")
+					.replaceAll("@magnata", getConfig().getString("Magnata_atual"))
+					.replaceAll("@exmagnata", getConfig().getString("Magnata-antigo"))
+					, 5, 40, 5); //Tempo do subtitle.
+		}
+		Bukkit.getServer().broadcastMessage("");
+		Bukkit.getServer().broadcastMessage("§2"+ getConfig().getString("Prefixo-Do-Plugin") + " " + getConfig().getString("msg-magnata-logou").replaceAll("@magnata", getConfig().getString("Magnata_atual")).replaceAll("@exmagnata", getConfig().getString("Magnata-antigo")));
+		Bukkit.getServer().broadcastMessage("");
+     		
+		}
+	}
+	@EventHandler
+	public void Magnata_deslogando(PlayerQuitEvent e){
+		Player p = e.getPlayer();
+		if(p.getName().equalsIgnoreCase(getConfig().getString("Magnata_atual"))){
+		Collection<? extends Player> list = Bukkit.getOnlinePlayers();
+		for (Player p2 : list) {
+			TTA_Methods.sendTitle(// API
+					p2, getConfig().getString("title-magnata-deslogou")
+					.replaceAll("@magnata", getConfig().getString("Magnata_atual"))
+					.replaceAll("@exmagnata", getConfig().getString("Magnata-antigo"))
+					, 5, 40, 5, //Tempo do title.
+					getConfig().getString("subtitle-magnata-deslogou")
+					.replaceAll("@magnata", getConfig().getString("Magnata_atual"))
+					.replaceAll("@exmagnata", getConfig().getString("Magnata-antigo"))
+					, 5, 40, 5); //Tempo do subtitle.
+		}
+		Bukkit.getServer().broadcastMessage("");
+		Bukkit.getServer().broadcastMessage("§2"+ getConfig().getString("Prefixo-Do-Plugin") + " " + getConfig().getString("msg-magnata-deslogou").replaceAll("@magnata", getConfig().getString("Magnata_atual")).replaceAll("@exmagnata", getConfig().getString("Magnata-antigo")));
+		Bukkit.getServer().broadcastMessage("");
+		}
 	}
 }
  
